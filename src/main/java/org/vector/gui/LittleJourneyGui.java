2783bf9055092af4dc2f98ca0fdf7e998e98b6b1
ihorstepura <igor.stepura96@gmail.com>
2020-10-30 11:29:44 +0200

    add some changes in gui

--- src/main/java/org/vector/gui/LittleJourneyGui.java
+++ src/main/java/org/vector/gui/LittleJourneyGui.java
@@ -5,8 +5,11 @@ import org.vector.mock.TicketFactory;
 import org.vector.utils.DataSelector;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class LittleJourneyGui extends JDialog {
@@ -20,13 +23,27 @@ public class LittleJourneyGui extends JDialog {
     private JTextField minTime;
     private JTextField maxTime;
     private JTextArea resultText;
+    private JLabel depLabel;
 
     public LittleJourneyGui() {
 
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonSearch);
-        setBounds(1300, 400, 1000, 1000);
+        setBounds(1000, 400, 1300, 1000);
+
+        Font bigFontTR = new Font("TimesRoman", Font.BOLD, 30);
+        Font littleFontTR = new Font("TimesRoman", Font.ITALIC, 20);
+
+        arrivalStation.setFont(bigFontTR);
+        departureStation.setFont(bigFontTR);
+        buttonSearch.setFont(bigFontTR);
+        minCost.setFont(bigFontTR);
+        maxCost.setFont(bigFontTR);
+        minTime.setFont(bigFontTR);
+        maxTime.setFont(bigFontTR);
+        resultText.setFont(littleFontTR);
+
 
         buttonSearch.addActionListener(new ActionListener() {
 
@@ -41,15 +58,56 @@ public class LittleJourneyGui extends JDialog {
 
         List<Ticket> tickets = createRandomTickets();
 
-        List<Ticket> selected = DataSelector.selectByRoute(tickets, departureStation.getText(), arrivalStation.getText());
+        String departure = departureStation.getText();
+        String arrival = arrivalStation.getText();
 
-        describeList(selected);
+        String minimalCost = minCost.getText();
+        String maximalCost = maxCost.getText();
 
+        //Date minimalTime = new Date(minTime.getText());
+        //Date maximalTime = new Date(maxTime.getText());
+
+
+        if (departure.equals("") || arrival.equals("")) {
+
+            if (minimalCost.equals("") || maximalCost.equals("")) {
+
+                resultText.append("Данных не обнаружено");
+            } else {
+
+                try {
+
+                    List<Ticket> selectedByCost = DataSelector.selectByPrice(
+                            tickets, Integer.parseInt(minimalCost), Integer.parseInt(maximalCost));
+                    describeList(selectedByCost);
+
+                } catch (Exception e) {
+
+                    resultText.append("Данных не обнаружено");
+                }
+            }
+
+        } else {
+
+
+            List<Ticket> selectedByRoute = DataSelector.selectByRoute(tickets, departure, arrival);
+            if (!minimalCost.equals("") || !maximalCost.equals("")) {
+
+                List<Ticket> selectedByCost = DataSelector.selectByPrice(
+                        selectedByRoute, Integer.parseInt(minimalCost), Integer.parseInt(maximalCost));
+
+                describeList(selectedByCost);
+            } else {
+
+                describeList(selectedByRoute);
+            }
+        }
     }
+
 
     public void showWindow() {
         LittleJourneyGui dialog = new LittleJourneyGui();
-        dialog.pack();
+        //dialog.pack();
         dialog.setVisible(true);
         System.exit(0);
     }
@@ -69,7 +127,7 @@ public class LittleJourneyGui extends JDialog {
 
         for (Ticket ticket : tickets) {
 
-            resultText.append(ticket.toString() + "\n"); //setText(ticket.toString());
+            resultText.append(ticket.toString() + "\n");
 
         }
     }
