3072f138cba14895ecfc6cc5e65d78a822fcac6a
ihorstepura <igor.stepura96@gmail.com>
2020-12-07 11:39:35 +0200

    LJ-5 added spring framework support to project (spring-data, spring-web, spring-security) and gradle support

---
+++ src/main/java/org/vector/littlejourney/Application.java
@@ -0,0 +1,12 @@
+package org.vector.littlejourney;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
