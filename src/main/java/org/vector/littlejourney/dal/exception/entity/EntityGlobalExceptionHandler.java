ae7f1c418ae68b363b97fbdf484dab7f3fb5e138
ihorstepura <igor.stepura96@gmail.com>
2020-12-21 22:54:17 +0200

    LJ-5 added global exception handler

---
+++ src/main/java/org/vector/littlejourney/dal/exception/entity/EntityGlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package org.vector.littlejourney.dal.exception.entity;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class EntityGlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<EntityIncorrectData> handleException(Exception exception) {
+
+        EntityIncorrectData data = new EntityIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/org/vector/littlejourney/dal/exception/entity/EntityIncorrectData.java
@@ -0,0 +1,9 @@
+package org.vector.littlejourney.dal.exception.entity;
+
+import lombok.Data;
+
+@Data
+public class EntityIncorrectData {
+
+    private String info;
+}
